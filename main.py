0fb700e64268a0a0850d249d81cb75e391b303ec
María Lourdes Puigdengolas Lanciani <89802699+LourdesPuigdengolas@users.noreply.github.com>
2023-06-25 23:40:03 -0300

    login design

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
- return render_template('index.html')
+    return render_template('index.html') # esto me lleva a la pagina index.html y asi puedo hacer con las otras
+    #lugo de validar el usuario me tendría que dirigir al html de preceptor
+
+#caso de ejemplo para login luego de presionar el boton ingresar
+@app.route('/login')
+def login():
+    isLoged = gestor.verifyUser()
+    if isLoged:
+        return render_template('preceptor.html')
+    else:
+        return render_template('index.html')#mostrar el error en la pagina de login
+
 if __name__ == '__main__':
- app.run()
+    app.run()
