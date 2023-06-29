98102ed30dde9f8c5199ee285f0df60a25f9d844
María Lourdes Puigdengolas Lanciani <89802699+LourdesPuigdengolas@users.noreply.github.com>
2023-06-28 21:39:08 -0300

    front registrar asistencia

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, redirect, request
 
+
 app = Flask(__name__)
 app.secret_key='CLAVE SECRETA'
 
@@ -34,6 +35,39 @@ def logout():
     return redirect('/')
 
 
+'''@app.route('/listarAsistencias', methods=['GET','POST'])
+def listarAsistencias():
+    if request.method == 'GET':
+        return redirect('/preceptor')
+    else:
+        asistencias = Asistencia.query.all()
+        return render_template('listarAsistencias.html', asistencias=asistencias)'''
+
+@app.route('/listarCursoPorPreceptor', methods=['GET','POST'])
+def listarCursoPorPreceptor():
+    if request.method == 'GET':
+        #cursos = Curso.query.all()
+        return render_template('listaCursos.html')
+
+    
+@app.route('/listarEstudiantesPorCurso', methods=['GET','POST'])
+def listarEstudiantesPorCurso():
+    if request.method == 'GET':
+        estudiantes = Estudiante.query.all()
+        return render_template('listarEstudiantesPorCurso.html', estudiantes=estudiantes)
+
+@app.route('/agregarAsistencia', methods=['GET','POST'])
+def agregarAsistencia():
+    if request.method == 'GET':
+        idEstudiante = request.form['idEstudiante']
+        fecha = request.form['fecha']
+        estado = request.form['estado']
+        asistencia = Asistencia(idEstudiante, fecha, estado)
+        db.session.add(asistencia)
+        db.session.commit()
+        return redirect('/preceptor')
+    else:
+        return render_template('agregarAsistencia.html')
 
 
 if __name__ == '__main__':
